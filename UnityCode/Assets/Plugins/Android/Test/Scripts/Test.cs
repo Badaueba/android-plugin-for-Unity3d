83973a01f49566b7013d832f72e6aeda84a3923c
badaueba <alan.develop@outlook.com>
2016-06-02 23:06:10 -0300

    fix debuging messages

--- UnityCode/Assets/Plugins/Android/Test/Scripts/Test.cs
+++ UnityCode/Assets/Plugins/Android/Test/Scripts/Test.cs
@@ -2,6 +2,8 @@
 using UnityEngine.UI;
 using System.Collections;
 
+//#if !UNITY_EDITOR && UNITY_ANDROID 
+
 /// <summary>
 /// Test.
 /// Access android library methods
@@ -9,6 +11,8 @@ using System.Collections;
 public class Test : MonoBehaviour {
 	public Text debugMessage;
 	private bool flashActive;
+	private AndroidJavaClass unity;
+	AndroidJavaObject currentActivity;
 	/// <summary>
 	/// Actives the flashlight.
 	/// Method to call "ActiveFlashLight" that 
@@ -16,47 +20,42 @@ public class Test : MonoBehaviour {
 	/// </summary>
 	/// 
 	void Awake () {
-		Debug ();
-	}
-
-	public void ActiveFlashlight(){
-		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
-		currentActivity.Call ("ActiveFlashlight", "Flashlight ON");
-
+		unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
+		currentActivity = GetCurrentActivity ();
 		debugMessage.text = currentActivity.Call<string> ("debugMessage");
-
 	}
+	//Access unityPlayer activity in library to get current activity running on device
+	AndroidJavaObject GetCurrentActivity () {
+		return unity.GetStatic<AndroidJavaObject> ("currentActivity");
+	}
+	//Turn On flashlight camera
+	public void ActiveFlashlight(){
+		currentActivity = GetCurrentActivity ();
+		currentActivity.Call ("ActiveFlashlight", "Flashlight ON");
+		debugMessage.text = currentActivity.Call<string> ("debugMessage");
+	}
+	//Turn Off flashlight camera
 	public void DeactiveFlashlight () {
-		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		currentActivity = GetCurrentActivity ();
 		currentActivity.Call ("deactiveFlashlight", "Flashlight OFF");
 		debugMessage.text = currentActivity.Call<string> ("debugMessage");
 	}
-
+	//toggle ActiveFlashlight/DeactiveFlahsligh Call 
 	public void ManageFlashlight () {
-		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
+		currentActivity = GetCurrentActivity ();
 		flashActive = currentActivity.Call <bool> ("isActiveFlashlight");
 		if (flashActive)
 			DeactiveFlashlight ();
 		else
 			ActiveFlashlight ();
 	}
-
+	//Share intent Call
 	public void ShareText(){
-		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
-		currentActivity.Call ("shareText", 
-			"Access native android functionality with a library (plugin) in Unity 3d", 
-			"codigo em: https://github.com/Badaueba/share-plugin-for-Unity3d");
-		Debug ();
-	}
-
-	public void Debug(){
-		AndroidJavaClass unity = new AndroidJavaClass ("com.unity3d.player.UnityPlayer");
-		AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject> ("currentActivity");
-		debugMessage.text = currentActivity.Call<string> ("debugMessage");
+		currentActivity = GetCurrentActivity ();
+		string subject = "Access native android functionality with a library (plugin) in Unity 3d";
+		string body = "Available in: https://github.com/Badaueba/share-plugin-for-Unity3d";
+		currentActivity.Call ("shareText", subject, body);
 	}
 
 }
+//#endif
